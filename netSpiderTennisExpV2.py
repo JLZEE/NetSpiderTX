31e4ac3981b82da4a60b25eac2944383e6ee3cf5
Jialei Zheng <jlzhengee@outlook.com>
2017-02-14 14:51:27 -0800

    Update netSpiderTennisExpV2.py

--- netSpiderTennisExpV2.py
+++ netSpiderTennisExpV2.py
@@ -9,8 +9,8 @@ import linecache
 # Tennis Express, user need to enter the search keywords to run the program
 # Program Name: Tennis Express Net Spider
 # Version: 0.2
-# Author: Gerry.Z
-# Time: 2017-02-14
+# Author: Gerry.Z, Zhou.L
+# Time: 2017-02-13
 # Usage: run this code in terminal, and type in product you wants to search
 # Output: a folder contains inform of all products from search result
 # ---------------------------------------------------------------------------
@@ -177,7 +177,7 @@ print u"""
 ---------------------------------------
     Name:     Net Spider TennisExpress
     Version:  0.2
-    Author:   Gerry.Z
+    Author:   Gerry.Z, Zhou.L
     Language: Python 2.7
 ---------------------------------------
 """
