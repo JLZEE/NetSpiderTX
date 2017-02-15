42e08bff1c19760e3f98cf534896d10b4da3eb89
Jialei Zheng <jlzhengee@outlook.com>
2017-02-15 09:50:43 -0800

    Update netSpiderTennisExpV2.py

--- netSpiderTennisExpV2.py
+++ netSpiderTennisExpV2.py
@@ -9,8 +9,8 @@ import linecache
 # Tennis Express, user need to enter the search keywords to run the program
 # Program Name: Tennis Express Net Spider
 # Version: 0.2
-# Author: Gerry.Z, Zhou.L
-# Time: 2017-02-13
+# Author: Gerry.Z
+# Time: 2017-02-14
 # Usage: run this code in terminal, and type in product you wants to search
 # Output: a folder contains inform of all products from search result
 # ---------------------------------------------------------------------------
@@ -23,6 +23,8 @@ class NetSpider:
         self.search_key_word = None
         self.search_result_page_code_txt = 'page_code.txt'
         self.search_result_page_code_html = 'page_code.html'
+        self.image_size = None
+
 
     def searchKeyWordTrans(self, inputStr):
         # check if input string is empty
@@ -37,6 +39,7 @@ class NetSpider:
             outputStr = inputStr.replace(' ', '-')
             self.search_key_word = outputStr
         return outputStr
+
 
     def getPage(self, keyword):
         myUrl = 'https://www.tennisexpress.com/all-products/browse/keyword/' + keyword
@@ -57,6 +60,7 @@ class NetSpider:
         page_code_file = open(self.search_result_page_code_txt, 'w')
         page_code_file.write(myPage)
         page_code_file.close()
+
 
     def exploreWeb(self, keyword):
 
@@ -162,13 +166,75 @@ class NetSpider:
         page_code_file_txt.close()
 
 
+    def getProductsImg(self, homeFolderPath, productsLinkList):
+        # this method is used to collecting all products image from the source web code
+        # first check the image size
+        if not (self.image_size):
+            size = 100
+        else:
+            size = self.image_size
+
+        for productsLink in productsLinkList:
+            # create a folder to store image
+            productsName = productsLink.split('/')[-1]
+            productsNamePath = homeFolderPath + '/' + productsName
+            productsImgPath = productsNamePath + '/' + productsName + '_img'
+            if not os.path.exists(productsImgPath):
+                os.makedirs(productsImgPath)
+
+            # scan source code to find products image
+            pageFileName = productsNamePath + '/page_' + productsName + '.txt'
+
+            productsImgLinkList = []
+            with open(pageFileName) as pgf:
+                lines = pgf.readlines()
+                for line in lines:
+                    matchTest = re.match('.*?<a id=\"zoomer\".*?|.*?<a class=\"altImage\".*?', str(line))
+                    if (matchTest != None):
+                        productsImgLink = re.compile(r'\"+').split(str(line))[3]
+                        productsImgLinkList.append(productsImgLink)
+                        #print productsImgLink
+
+            # put these source in a txt file and download them
+            productsImgLinkFile = open(productsImgPath + '/' + productsName + '_imgLinks.txt', 'w')
+            productsImgLinkFile.write('imageLinks\n')
+            productsImgLinkFile.close()
+
+            productsImgLinkFile = open(productsImgPath + '/' + productsName + '_imgLinks.txt', 'a')
+            for productsImgLink in productsImgLinkList:
+                # write link in a txt file
+
+                productsImgLinkFile.write(productsImgLink + '\n')
+                # download images
+                imgFileName = productsImgPath + '/' + productsImgLink.split('/')[-1]
+
+                try:
+                    if not os.path.exists(imgFileName):
+                        urllib.urlretrieve(productsImgLink, imgFileName)
+                    else:
+                        continue
+                except Exception as e:
+                    print(e)
+                    continue
+                # print productsImgLink
+                # print imgFileName
+
+            productsImgLinkFile.close()
+
+
     def start(self, inputStr):
         self.enable = True
         keyword = self.searchKeyWordTrans(inputStr)
         if keyword:
             self.getPage(keyword)
             print 'Page file saved'
-            self.exploreWeb(keyword)
+            # get the home path and all products link list
+            (homeFolderPath, productsLinkList) = self.exploreWeb(keyword)
+
+            # get images of all products
+            self.getProductsImg(homeFolderPath, productsLinkList)
+
+            print 'Program finish'
         else:
             print 'Input error, please try again'
 
@@ -177,7 +243,7 @@ print u"""
 ---------------------------------------
     Name:     Net Spider TennisExpress
     Version:  0.2
-    Author:   Gerry.Z, Zhou.L
+    Author:   Gerry.Z
     Language: Python 2.7
 ---------------------------------------
 """
